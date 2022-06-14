457ade692f0c8a2c7d94bb1f1390e0c74ef8817a
Kseniia_Lebedeva <kseniia_lebedeva@epam.com>
2022-06-14 23:33:11 +0200

    started a new project: employees

---
+++ src/components/app/app.js
@@ -0,0 +1,11 @@
+import './app.css';
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Hello, world!</h1>
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
-import React, {StrictMode} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {MyComponent, Header} from "./App";
-
-const elem = <h2>Hello World!</h2>;
-const text = "some text";
-/*create element w/o jsx*/
-// const elem = React.createElement('h2', null, 'Hello World!!!');
-const elem2 = (
-    <div>
-        <h1>Text:{text}</h1>
-        <input type="text"/>
-        <button>click</button>
-    </div>
-);
+import App from "./components/app/app";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // elem2
-    // <React.StrictMode>
-    //   <App/>
-    // </React.StrictMode>
-    <StrictMode>
-        <MyComponent/>
-    </StrictMode>
+    <App/>
 );
-reportWebVitals();
