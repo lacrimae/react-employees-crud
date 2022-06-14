c50785a383626c204e7e899b7013dfb5f4a3360d
Kseniia_Lebedeva <kseniia_lebedeva@epam.com>
2022-06-14 23:01:23 +0200

    practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Component} from "react";
+// components:
+const Header = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    );
 }
 
+const Field = () => {
+    return (
+        <div>
+            <input placeholder="Type here" type="text"/>
+        </div>
+    );
+}
+
+function Button() {
+    // const res = () => {
+    //     return 'log in';
+    // };
+    // const p = <p>log in</p>;
+    const text = 'log in';
+    const logged = true;
+    return (
+        <div>
+            <button>{logged ? 'Enter' : text}</button>
+        </div>
+    );
+}
+
+class MyComponent extends Component {
+    render() {
+        const holder = 'Type here';
+        const style = {
+            backgroundColor: 'pink',
+            width: '300px'
+        };
+
+        return (
+            <div>
+                <input
+                    style={style}
+                    type={'hi'}
+                    placeholder={holder}
+                />
+            </div>
+        );
+    }
+}
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Field/>
+            <Button/>
+        </div>
+    );
+}
+
+export {Header, MyComponent};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {MyComponent, Header} from "./App";
 
 const elem = <h2>Hello World!</h2>;
+const text = "some text";
+/*create element w/o jsx*/
+// const elem = React.createElement('h2', null, 'Hello World!!!');
+const elem2 = (
+    <div>
+        <h1>Text:{text}</h1>
+        <input type="text"/>
+        <button>click</button>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    elem
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+    // elem2
+    // <React.StrictMode>
+    //   <App/>
+    // </React.StrictMode>
+    <StrictMode>
+        <MyComponent/>
+    </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
