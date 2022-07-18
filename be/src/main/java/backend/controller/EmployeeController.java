61063ae8a0fd440f4c6e41123023725f2ad753bf
Kseniia_Lebedeva <kseniia_lebedeva@epam.com>
2022-07-18 19:55:36 +0200

    feature: fixed duplicated endpoints in swagger ui by adding a property in application.yml

--- be/src/main/java/backend/controller/EmployeeController.java
+++ be/src/main/java/backend/controller/EmployeeController.java
@@ -6,22 +6,20 @@ import backend.model.EmployeeRequestDto;
 import backend.model.EmployeeResponseDto;
 import backend.service.EmployeeService;
 import static backend.support.EmployeeConstraints.BASE_ENDPOINT_MAPPING;
+import static java.util.Collections.emptyList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Controller
-//@RequestMapping()
+@RestController
 @RequestMapping(BASE_ENDPOINT_MAPPING)
 @RequiredArgsConstructor
 public class EmployeeController implements EmployeeApi {
     private final EmployeeService employeeService;
 
-    @Override
-    public ResponseEntity<EmployeeResponseDto> createEmployee(EmployeeRequestDto dto) {
-        return null;
+    @Override public ResponseEntity<EmployeeResponseDto> createEmployee(EmployeeRequestDto employeeRequestDto) {
+        return EmployeeApi.super.createEmployee(employeeRequestDto);
     }
 
     @Override public ResponseEntity<Void> deleteEmployee(String employeeId) {
@@ -30,7 +28,8 @@ public class EmployeeController implements EmployeeApi {
 
     @Override
     public ResponseEntity<List<EmployeeResponseDto>> getEmployee() {
-        return ResponseEntity.ok(employeeService.getAllEmployees());
+        //        return ResponseEntity.ok(employeeService.getAllEmployees());
+        return ResponseEntity.ok(emptyList());
     }
 
     @Override
