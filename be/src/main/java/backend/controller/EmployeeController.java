63e98cb0692dd35c82cc953e48c28985153babbc
Kseniia_Lebedeva <kseniia_lebedeva@epam.com>
2022-07-18 13:28:44 +0200

    feature: added openapi ui, todo: remove duplicated endpoints

--- be/src/main/java/backend/controller/EmployeeController.java
+++ be/src/main/java/backend/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package backend.controller;
 
 import java.util.List;
 
+import backend.model.EmployeeRequestDto;
 import backend.model.EmployeeResponseDto;
 import backend.service.EmployeeService;
 import static backend.support.EmployeeConstraints.BASE_ENDPOINT_MAPPING;
@@ -12,13 +13,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+//@RequestMapping()
 @RequestMapping(BASE_ENDPOINT_MAPPING)
 @RequiredArgsConstructor
 public class EmployeeController implements EmployeeApi {
     private final EmployeeService employeeService;
 
     @Override
+    public ResponseEntity<EmployeeResponseDto> createEmployee(EmployeeRequestDto dto) {
+        return null;
+    }
+
+    @Override public ResponseEntity<Void> deleteEmployee(String employeeId) {
+        return null;
+    }
+
+    @Override
     public ResponseEntity<List<EmployeeResponseDto>> getEmployee() {
         return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    @Override
+    public ResponseEntity<EmployeeResponseDto> updateEmployee(String employeeId, EmployeeRequestDto employeeRequestDto) {
+        return null;
     }
 }
